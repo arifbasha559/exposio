4e439f726b32987d8ee2c02b0915cedafdb12ce5
Arif Basha <arifbasha559@gmail.com>
2025-03-25 02:08:52 +0530

    initialize React project with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-[#F4C2C2] px-20 min-h-screen mb-96 max-w-screen relative  bg-cover bg-center bg-fixed"> 
+    <div className="navbar w-full flex justify-center sticky top-0 left-0 backdrop-blur-xs h-fit " >
+      
+      <h1 className='text-5xl text-s font-extrabold  py-5 font-exposio text-transparent bg-gradient-to-r w-fit bg-clip-text from-[#ff0081] to-[#ff084a] title '>Exposio</h1>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default App
