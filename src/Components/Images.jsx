15dd01a6723eb98849eb537fae3e24adcfb867bd
Arif Basha <arifbasha559@gmail.com>
2025-03-27 16:52:25 +0530

    remove likes API handler and related state management from Images and Videos components; update like display logic to use static data

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -1,39 +1,7 @@
-import { useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import { Data } from "./Data";
 const Images = () => {
-  const [likes, setLikes] = useState(
-    Data.reduce((acc, image) => {
-      acc[image.webformatURL] = image.likes;
-      return acc;
-    }, {})
-  );
-  const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5173/api/likes.js" // Change 3000 if using another port for Vercel dev
-    : "/api/likes.js";
-
-const handleLike = async (id,type) => {
-  try {
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ id , type}),
-      });
-      
-      if (response.ok) {
-        setLikes((prevLikes) => ({
-          ...prevLikes,
-          [id]: prevLikes[id] + 1,
-        }));
-      } else {
-        console.error("Failed to update like count");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
+ 
   return (
     <div className="py-16 sm:px-0 flex flex-col items-center gap-5 min-h-screen">
       <h1 className="text-5xl font-extrabold py-5 text-transparent bg-gradient-to-r w-fit bg-clip-text from-[#ff0081] to-[#ff084a]">
@@ -57,14 +25,14 @@ const handleLike = async (id,type) => {
               <div className="hidden group-hover:flex flex-col items-center justify-center absolute inset-0 transition duration-500">
                 <button
                   className="absolute top-2 left-2 flex items-stretch w-[100px] h-[35px] border-none rounded-[5px] overflow-hidden shadow-md cursor-pointer bg-transparent hover:[&_.leftContainer]:bg-[rgb(219,0,0)]"
-                  onClick={() => handleLike(image.webformatURL,"image")}
+                 
                 >
                   <span className="leftContainer flex items-center text-xs justify-center w-[60%] h-full bg-[rgb(238,0,0)] gap-2">
                     <FaHeart className="text-white text-sm" />
                     <span className="like text-white font-semibold">Like</span>
                   </span>
                   <span className="likeCount flex items-center text-xs justify-center w-[40%] h-full text-[#ff084a] font-semibold bg-white">
-                    {likes[image.webformatURL]}
+                    {image.likes}
                   </span>
                 </button>
                 <div className="absolute right-1   bottom-2">
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,43 +1,9 @@
 import { FaArrowDown, FaHeart } from "react-icons/fa";
 
 import { Data } from "./VideosData";
-import { useState } from "react";
 
 const Videos = () => {
-  const [likes, setLikes] = useState(
-    Data.reduce((acc, image) => {
-      acc[image.id] = image.likes;
-      return acc;
-    }, {})
-  );
-  const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5173/api/likes.js" // Change 3000 if using another port for Vercel dev
-    : "/api/likes.js";
-
-const handleLike = async (id,type) => {
-  try {
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ id , type}),
-      });
-      const data = await response;
-    console.log("Server Response:", data);
-
-      if (response.ok) {
-        setLikes((prevLikes) => ({
-          ...prevLikes,
-          [id]: prevLikes[id] + 1,
-        }));
-      } else {
-        console.error("Failed to update like count");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-  
+ 
   return (
     <div className="py-16 flex flex-col items-center gap-5 min-h-screen max-w-[2080px] mx-auto">
       <h1 className="text-4xl md:text-5xl font-extrabold py-5 text-transparent bg-gradient-to-r w-fit bg-clip-text from-[#ff0081] to-[#ff084a]">
@@ -75,14 +41,14 @@ const handleLike = async (id,type) => {
               <div className="hidden group-hover:flex group-focus:flex flex-col items-center justify-center absolute inset-0  transition duration-500">
                 <button
                   className="absolute top-2 left-2 flex items-stretch w-[100px] h-[35px] border-none rounded-[5px] overflow-hidden shadow-md cursor-pointer bg-transparent hover:[&_.leftContainer]:bg-[rgb(219,0,0)]"
-                  onClick={() => handleLike(image.id,"video")}
+                  
                 >
                   <span className="leftContainer flex items-center text-xs justify-center w-[60%] h-full bg-[rgb(238,0,0)] gap-2">
                     <FaHeart className="text-white text-sm" />
                     <span className="like text-white font-semibold">Like</span>
                   </span>
                   <span className="likeCount flex items-center text-xs justify-center w-[40%] h-full text-[#ff084a] font-semibold bg-white">
-                    {likes[image.id]}
+                    {image.likes}
                   </span>
                 </button>
                 <div className="absolute right-1   bottom-2">
